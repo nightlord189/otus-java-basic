1a8627daab4b6a0b220e43ed1ac95c57561bbc27
Alexey Buravov <aburavov189@gmail.com>
2025-09-14 20:46:52 +0500

    HW06 (#6)
    * HW06
    
    * Fix

---
+++ src/main/java/org/aburavov/otus/java/basic/hw06/Cat.java
@@ -0,0 +1,32 @@
+package org.aburavov.otus.java.basic.hw06;
+
+public class Cat {
+    private final String name;
+    private final int appetite;
+    private boolean isHungry = true;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public boolean eat(Plate plate) {
+        if (plate.decreaseFood(appetite)) {
+            this.isHungry = false;
+            System.out.println(name + " ate");
+            return true;
+        } else {
+            System.out.println(name + " not enough food to eat :(");
+            return false;
+        }
+    }
+
+    public void info() {
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return name + ", appetite: " + appetite + ", isHungry: " + isHungry;
+    }
+}
