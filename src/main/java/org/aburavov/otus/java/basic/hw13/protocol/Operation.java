83cbbf2f8ac4660d105699741e1655a8bc06bda3
Alexey Buravov <aburavov189@gmail.com>
2025-10-16 22:59:26 +0600

    HW13 (#13)
    * HW13
    
    * Fix const naming

---
+++ src/main/java/org/aburavov/otus/java/basic/hw13/protocol/Operation.java
@@ -0,0 +1,27 @@
+package org.aburavov.otus.java.basic.hw13.protocol;
+
+public enum Operation {
+    PLUS("+"),
+    MINUS("-"),
+    DIVIDE("/"),
+    MULTIPLY("*");
+
+    private final String symbol;
+
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public static Operation fromSymbol(String symbol) {
+        for (Operation op : values()) {
+            if (op.symbol.equals(symbol)) {
+                return op;
+            }
+        }
+        return null; // или бросить исключение, или вернуть Optional
+    }
+}
---
+++ src/main/java/org/aburavov/otus/java/basic/hw13/protocol/Parameter.java
@@ -0,0 +1,10 @@
+package org.aburavov.otus.java.basic.hw13.protocol;
+
+public enum Parameter {
+    REQUEST_TYPE,
+    NUMBERS,
+    OPERATION,
+    RESULT,
+    STATUS,
+    ERROR
+}
