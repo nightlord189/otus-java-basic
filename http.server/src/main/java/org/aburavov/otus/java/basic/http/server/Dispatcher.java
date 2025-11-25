dd10af700840c1ee9abfdcb2629cafae4a0bd4bc
Alexey Buravov <aburavov189@gmail.com>
2025-11-26 00:39:21 +0500

    HW23: ThreadPool

--- http.server/src/main/java/org/aburavov/otus/java/basic/http/server/Dispatcher.java
+++ http.server/src/main/java/org/aburavov/otus/java/basic/http/server/Dispatcher.java
@@ -2,6 +2,8 @@ package org.aburavov.otus.java.basic.http.server;
 
 import org.aburavov.otus.java.basic.http.server.exceptions_handling.BadRequestException;
 import org.aburavov.otus.java.basic.http.server.processors.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -10,6 +12,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Dispatcher {
+    private final static Logger logger = LogManager.getLogger(Dispatcher.class.getName());
+
     private Map<String, RequestProcessor> routes;
     private RequestProcessor defaultNotFoundProcessor;
 
@@ -23,12 +27,15 @@ public class Dispatcher {
 
     public void execute(HttpRequest request, OutputStream output) throws IOException {
         if (!routes.containsKey(request.getRoutingKey())) {
+            logger.warn("Not found: {}", request.getRoutingKey());
             defaultNotFoundProcessor.execute(request, output);
             return;
         }
         try {
             routes.get(request.getRoutingKey()).execute(request, output);
         } catch (BadRequestException e) {
+            logger.error("Bad request: {}", String.valueOf(e));
+
             String response = "" +
                     "HTTP/1.1 400 Bad Request\r\n" +
                     "Content-Type: text/html;charset=utf-8\r\n" +
@@ -36,6 +43,8 @@ public class Dispatcher {
                     "<html><body><h1>BAD REQUEST: " + e.getMessage() + "</h1></body></html>";
             output.write(response.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
+            logger.error("Exception on processing request: {}", String.valueOf(e));
+
             String response = "" +
                     "HTTP/1.1 500 Internal Server Error\r\n" +
                     "Content-Type: text/html;charset=utf-8\r\n" +
